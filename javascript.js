ff882affe11883c44833323b0df2662c6cb35ac5
Naomi Tan <69682497+naomi-tan@users.noreply.github.com>
2023-12-11 11:13:34 +0000

    add playRound function

--- javascript.js
+++ javascript.js
@@ -9,4 +9,70 @@ function getComputerChoice(){
     return computerChoice;
 }
 
-getComputerChoice();
+function getPlayerChoice(){
+    // Get user input and convert string to all lower case
+    let playerSelection = prompt("Enter rock, paper, or scissors: ").toLowerCase();
+    console.log("Player Choice: ", playerSelection)
+    return playerSelection;
+}
+
+function playRound(playerSelection, computerSelection){
+    // Return outcome of the round
+    let outcome;
+    if (playerSelection === computerSelection){ // check for draw first
+        outcome = "draw"
+    }
+    else { // get outcome if not a draw
+        switch (playerSelection){
+            case "rock":
+                if (computerSelection === "paper"){
+                    outcome = "lose"
+                }
+                else if (computerSelection === "scissors"){
+                    outcome = "win"
+                }
+                break;
+            case "paper":
+                if (computerSelection === "scissors"){
+                    outcome = "lose"
+                }
+                else if (computerSelection === "rock"){
+                    outcome = "win"
+                }
+                break;
+            case "scissors":
+                if (computerSelection === "rock"){
+                    outcome = "lose"
+                }
+                else if (computerSelection === "paper"){
+                    outcome = "win"
+                }
+                break;
+            }
+    }
+    switch (outcome){ // use outcome to print to console
+        case "draw":
+            console.log("It's a draw - I demand a rematch!")
+            break;
+        case "win":
+            console.log("You win this time.. ", playerSelection, " beats ", computerSelection, ".")
+            break;
+        case "lose":
+            console.log("You lose.. ", computerSelection, " beats ", playerSelection, ".")
+            break;
+        }
+    return outcome;
+}
+
+function game(){
+    // Play best of 5 and keep score
+}
+
+let round = 0;
+let score = 0;
+let outcome;
+do{ // if outcome is a draw, replay round
+    const playerSelection = getPlayerChoice();
+    const computerSelection = getComputerChoice();
+    outcome = playRound(playerSelection, computerSelection);
+} while(outcome === "draw");
