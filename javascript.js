b68e4c74acc76d734e6edd46850bc7abc4d8550c
Naomi Tan <69682497+naomi-tan@users.noreply.github.com>
2023-12-11 11:50:21 +0000

    add game function and checkUserInput function

--- javascript.js
+++ javascript.js
@@ -1,8 +1,6 @@
-// Store possible choices as array of strings
-const choices = ["rock", "paper", "scissors"];
-
 function getComputerChoice(){
     // Return random option rock, paper or scissors when called
+    const choices = ["rock", "paper", "scissors"]; // store possible choices as array of strings
     let randInt = Math.round((Math.random() * 2)); // random integer 0-2
     let computerChoice = choices[randInt]; // map integer to array index
     console.log("Computer Choice: ", computerChoice)
@@ -11,9 +9,12 @@ function getComputerChoice(){
 
 function getPlayerChoice(){
     // Get user input and convert string to all lower case
-    let playerSelection = prompt("Enter rock, paper, or scissors: ").toLowerCase();
+    let playerSelection;
+    do{ // prompt again if user cancels prompt
+        playerSelection = prompt("Enter rock, paper, or scissors: ");
+    }while(checkUserInput(playerSelection) === "invalid")
     console.log("Player Choice: ", playerSelection)
-    return playerSelection;
+    return playerSelection.toLowerCase();
 }
 
 function playRound(playerSelection, computerSelection){
@@ -58,7 +59,7 @@ function playRound(playerSelection, computerSelection){
             console.log("You win this time.. ", playerSelection, " beats ", computerSelection, ".")
             break;
         case "lose":
-            console.log("You lose.. ", computerSelection, " beats ", playerSelection, ".")
+            console.log("Hah, you lose.. ", computerSelection, " beats ", playerSelection, ".")
             break;
         }
     return outcome;
@@ -66,13 +67,47 @@ function playRound(playerSelection, computerSelection){
 
 function game(){
     // Play best of 5 and keep score
+    let playerSelection;
+    let computerSelection;
+    let round = 0;
+    let score = 0;
+    let outcome;
+    while(round < 5){
+        console.log("ROUND ", round + 1)
+        do{ // if outcome is a draw, replay round
+            playerSelection = getPlayerChoice();
+            computerSelection = getComputerChoice();
+            outcome = playRound(playerSelection, computerSelection);
+        } while(outcome === "draw");
+        if (outcome === "win"){
+            ++score;
+        }
+        ++round; // increment round counter
+    }
+    if (score >= 3){
+        console.log("You win - I'll beat you next time!")
+    }
+    else{
+        console.log("I win - Better luck next time!")
+    }
 }
 
-let round = 0;
-let score = 0;
-let outcome;
-do{ // if outcome is a draw, replay round
-    const playerSelection = getPlayerChoice();
-    const computerSelection = getComputerChoice();
-    outcome = playRound(playerSelection, computerSelection);
-} while(outcome === "draw");
+function checkUserInput(userInput){
+    if (userInput === null){
+        console.log("Stop being a wimp and fight me!")
+        return "invalid";
+    }
+    if (userInput === ""){
+        console.log("Nervous? You entered nothing!")
+        return "invalid";
+    }
+    if ((userInput.toLowerCase() === "rock") || (userInput.toLowerCase() === "paper") || (userInput.toLowerCase() === "scissors")){
+        return "valid";
+    }
+    else{
+        console.log("Do I need to spell it for you?.. Invalid entry.")
+        return "invalid";
+    }
+}
+
+game();
